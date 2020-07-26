7c9e4f104eb583e98f030129ce9bf90f05eef676
Harshit Singh <harshit.harshit25@gmail.com>
2020-07-26 15:36:11 +0530

    BCG Commit

---
+++ FBullCowGame.h
@@ -0,0 +1,49 @@
+/*The gane logic(no view code or direct user interaction)
+The game is simply guess the word game based on Mastermind
+*/
+
+#pragma once
+#include <string>
+
+//to make Unreal friendly
+using FString = std::string;
+using int32 = int;
+
+struct FBullCowCount
+{
+	int32 Bulls = 0;
+	int32 Cows = 0;
+};
+
+enum class EGuessStatus
+{
+	Invalid_Status,
+	OK,
+	Not_Isogram,
+	Wrong_Length,
+	Not_Lowercase
+};
+
+class FBullCowGame
+{
+public:
+	FBullCowGame(); // constructor
+
+	int32 GetMaxTries() const;
+	int32 GetCurrentTry() const;
+	int32 GetHiddenWordLength() const;
+	bool IsGameWon() const;
+	EGuessStatus CheckGuessValidity(FString) const;
+
+	void Reset();
+	FBullCowCount SubmitValidGuess(FString);
+
+private:
+	// see constructor for initialisation
+	int32 MyCurrentTry;
+	FString MyHiddenWord;
+	bool bGameIsWon;
+
+	bool IsIsogram(FString) const;
+	bool IsLowercase(FString) const;
+};
